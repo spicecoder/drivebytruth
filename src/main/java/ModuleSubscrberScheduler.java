8fa4ce999266112788fa3b44d767db3de423f1ce
Pronab Pal <palpronab@gmail.com>
2018-11-26 00:58:33 +1100

    cleanup

--- src/main/java/ModuleSubscrberScheduler.java
+++ src/main/java/ModuleSubscrberScheduler.java
@@ -4,18 +4,11 @@ import java.util.concurrent.Flow;
 import java.util.concurrent.SubmissionPublisher;
 
 public class ModuleSubscrberScheduler implements Flow.Subscriber<TruthState> {
-    //it schedules an execution with a subscription alerady made
-    //on reciving subscribed truth state it schedules another"
-
 
     private Flow.Subscription subscription;
     static ExecutorService executor = Executors.newFixedThreadPool(10);
     static SubmissionPublisher<TruthState> resultPublisher = new SubmissionPublisher<TruthState>();
     static ModuleSubscrberScheduler mdx;
-    static TruthState currentTruth;
-    static public String userid_input;
-    static public String password_input;
-
 
     public static void main(String[] args) {
         mdx = new ModuleSubscrberScheduler();
@@ -41,9 +34,7 @@ public class ModuleSubscrberScheduler implements Flow.Subscriber<TruthState> {
             }
         };
 
-        //  ExecutorService executor = Executors.newFixedThreadPool(10);
         executor.execute(runnableTask);
-
 
 
     }
@@ -51,16 +42,14 @@ public class ModuleSubscrberScheduler implements Flow.Subscriber<TruthState> {
 
     @Override
     public void onSubscribe(Flow.Subscription subscription) {
-        System.out.println("Subscribed by in xecutor " + subscription.toString());
+
         this.subscription = subscription;
         this.subscription.request(1);
     }
 
     @Override
     public void onNext(TruthState item) {
-        //StarterModuleWithPublisher.resultPublisher.unsubscribe (this);
 
-        System.out.println("response receivedin onNext" + item.name);
         subscription.request(1);
 
         Runnable runnableTask = () -> {
@@ -73,10 +62,7 @@ public class ModuleSubscrberScheduler implements Flow.Subscriber<TruthState> {
             }
         };
 
-        //  ExecutorService executor = Executors.newFixedThreadPool(10);
         executor.execute(runnableTask);
-
-
 
 
     }
